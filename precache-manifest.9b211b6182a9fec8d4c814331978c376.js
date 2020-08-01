01622d377bd096db27297df8988b76368bea7a31
iheathers <shresthapritam7@gmail.com>
2020-08-01 22:40:56 +0545

    Updates

---
+++ precache-manifest.9b211b6182a9fec8d4c814331978c376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0803c244e16dc153bd09a52220c08b5",
+    "url": "/Glitch/index.html"
+  },
+  {
+    "revision": "e4b232bcecbf01029b4c",
+    "url": "/Glitch/static/js/2.c9a6ebeb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Glitch/static/js/2.c9a6ebeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbf0f4b8690950bed603",
+    "url": "/Glitch/static/js/main.3c9883d9.chunk.js"
+  },
+  {
+    "revision": "d6dacf463083554c4d9e",
+    "url": "/Glitch/static/js/runtime-main.7ab46177.js"
+  }
+]);
